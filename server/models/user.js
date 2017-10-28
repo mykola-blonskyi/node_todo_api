b17383a4a60795c08690cfa38ba21743ca643420
xomajke <nikolay.blonskiy@gmail.com>
2017-10-28 21:07:33 +0300

    add POST /users route

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 let User = mongoose.model('User', {
   email: {
     required: true,
     trim: true,
     type: String,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email!'
+    }
+    // validate: {
+    //   validator: (value) => {
+    //     return validator.isEmail(value);
+    //   },
+    //   message: '{VALUE} is not a valid email!'
+    // }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -89,6 +89,14 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  let new_user = new User(body);
+
+  new_user.save().then((user) => res.send(user)).catch((err) => res.status(400).send(err));
+});
+
 app.listen(port, () => console.log(`Started on port ${port}.`));
 
 module.exports = {app};
