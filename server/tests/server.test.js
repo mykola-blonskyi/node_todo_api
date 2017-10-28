ee85484706db8c22e8559b00a0bedc34b2442c2b
xomajke <nikolay.blonskiy@gmail.com>
2017-10-28 18:46:56 +0300

    test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,3 +103,28 @@ describe('GET /todos/:id', () => {
       .end(done);
   })
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove todo by id', (done) => {
+    request(app)
+      .delete(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      }).end(done)
+  });
+
+  it('Should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should return 404 for non-object id', (done) => {
+    request(app)
+      .delete(`/todos/123`)
+      .expect(404)
+      .end(done);
+  })
+});
