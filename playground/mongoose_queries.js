c12f1ae72e6cc793b29aa1b2f7d825917d64d67d
xomajke <nikolay.blonskiy@gmail.com>
2017-10-28 17:17:10 +0300

    add queries playground

---
+++ playground/mongoose_queries.js
@@ -0,0 +1,32 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// const id = '59f48d2f6397eb08ac7829a4';
+//
+// if(!ObjectID.isValid(id)){
+//   return console.log('id not valid');
+// }
+
+// Todo.find({_id: id}).then((todos) => {
+//   console.log('todos: ', todos);
+// });
+//
+// Todo.findOne({_id: id}).then((todo) => {
+//   console.log('todo: ', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('id not found');
+//   }
+//   console.log('find by id todo: ', todo);
+// }).catch((err) => console.log(err));
+
+User.findById('59f4745d4a43f9292430434d').then((user) => {
+  if(!user){
+    return console.log('no such user');
+  }
+  console.log('user: ', user);
+}, (err) => console.log(err));
