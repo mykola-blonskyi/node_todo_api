e490f8da8e23a2b37c9515f232ede8381f23c4e1
xomajke <nikolay.blonskiy@gmail.com>
2017-10-28 23:23:17 +0300

    hash user pass before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {id: 10};
+let pass = 'zxc123';
 
-let token = jwt.sign(data, 'h234g12');
-console.log('token: ', token);
-let decoded = jwt.verify(token, 'h234g12');
-console.log('decoded: ', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(pass, salt, (err, hash) => console.log(hash));
+// });
+
+let hashed_pass = '$2a$10$YI4SUvTmne7u512H5taS1u1WPZomkv/y75MNn3EVaXEGqD4gsbshK';
+
+bcrypt.compare(pass, hashed_pass, (err, res) => console.log(res));
+
+// let data = {id: 10};
+//
+// let token = jwt.sign(data, 'h234g12');
+// console.log('token: ', token);
+// let decoded = jwt.verify(token, 'h234g12');
+// console.log('decoded: ', decoded);
 // let msg = 'some text line';
 // let hash = SHA256(msg).toString();
 //
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
   email: {
@@ -72,6 +73,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function (next) {
+  let user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    });
+  } else {
+    next();
+  }
+});
 
 let User = mongoose.model('User', UserSchema);
 
