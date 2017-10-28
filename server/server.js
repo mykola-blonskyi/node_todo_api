0c32688f239200c69c3848fa4616bed5fd875335
xomajke <nikolay.blonskiy@gmail.com>
2017-10-28 17:39:06 +0300

    add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const body_parser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
     (todos) => res.send({todos}),
     (err) => res.status(400).send(err)
   );
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({Error: 'Id is not valid'});
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+
+    }
+
+    return res.send({todo});
+  }).catch((err) => res.status(400).send());
 });
 
 app.listen(3000, () => console.log('Started on port 3000.'));
